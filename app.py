98748a2151ea969247eeeb4b57d0b01e1737bef8
UtsavRaychaudhuri <UtsavRaychaudhuri@users.noreply.github.com>
2017-04-08 20:07:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import urllib
 import json
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request
 from flask import make_response
 
@@ -12,33 +12,26 @@ from flask import make_response
 app = Flask(__name__)
 
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/getData', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
 
     print("Request:")
     print(json.dumps(req, indent=4))
-
-    res = makeWebhookResult(req)
-
-    res = json.dumps(res, indent=4)
-    print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
-
-def makeWebhookResult(req):
+    data=[]
     if req.get("result").get("action") != "recommender.actions":
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
+
     exp = parameters.get("Experience")
     exp=''.join(exp)
-    skillset=parameters.get("Skillset")
-    skillset=''.join(skillset)
-    practice=parameters.get("Practice")
-    practice=''.join(practice)
-     if(exp=='2+ years' or exp=='3+ years' or exp=='4+ years' or exp=='1+ years' or exp=='SE'):
+
+    practice = parameters.get("Practice")
+    practice = ''.join(practice)
+    skillset = parameters.get("Skillset")
+    skillset = ''.join(skillset)
+    if(exp=='2+ years' or exp=='3+ years' or exp=='4+ years' or exp=='1+ years' or exp=='SE'):
         exp="SE"
     if (exp == '5+ years' or exp == '6+ years' or exp == '7+ years' or exp == 'SSE'):
         exp='SSE'
@@ -48,8 +41,9 @@ def makeWebhookResult(req):
         exp='ATM'
     if (exp == '11+ years' or exp == '12+ years' or  exp == 'Manager'):
         exp='Manager'
-
-    data=[]
+    print exp
+    print practice
+    print skillset
     data.append({'practice':'IBM','experience':'SE','skillset':'Java','Position':'02'})
     data.append({'practice': 'CIS', 'experience': 'SSE', 'skillset': 'python','Position':'01'})
     data.append({'practice': 'Convergens', 'experience': 'LSE', 'skillset': '.net','Position':'04'})
@@ -63,18 +57,13 @@ def makeWebhookResult(req):
         if (x['practice']==practice):
             if (x['experience']==exp ):
                 if x['skillset']==skillset:
-                    str="We have oppurtunities in " + practice + " and the position offered will be of " + exp  + ' and skill required is ' + skillset +' No.Of Position '+x['Position']
+                    str="We have oppurtunities in " + practice + " and the position offered will be of " + exp  + 'and skill required is ' + skillset +' No.Of Position '+x['Position']
                 else:
                     str="We have no such oppurtunities right now. But we will get back to you when we have such openings"
 
     #return jsonify({'result':str})
 
-    #speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
-
-    print("Response:")
-    print(str)
-
-    return {
+return {
         "speech": str,
         "displayText": str,
         #"data": {},
@@ -82,8 +71,7 @@ def makeWebhookResult(req):
         "source": "apiai-onlinestore-shipping"
     }
 
-
-if __name__ == '__main__':
+if _name_ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
     print "Starting app on port %d" % port
