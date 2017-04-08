a8ecb06d6236e8969e332134d2fe8d3ad686391e
UtsavRaychaudhuri <UtsavRaychaudhuri@users.noreply.github.com>
2017-04-08 20:51:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import urllib
 import json
 import os
 
-from flask import Flask, jsonify
+from flask import Flask
 from flask import request
 from flask import make_response
 
@@ -18,53 +18,66 @@ def webhook():
 
     print("Request:")
     print(json.dumps(req, indent=4))
-    data=[]
+
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeWebhookResult(req):
     if req.get("result").get("action") != "recommender.actions":
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
-
     exp = parameters.get("Experience")
     exp=''.join(exp)
-
-    practice = parameters.get("Practice")
-    practice = ''.join(practice)
-    skillset = parameters.get("Skillset")
-    skillset = ''.join(skillset)
-    if(exp=='2+ years' or exp=='3+ years' or exp=='4+ years' or exp=='1+ years' or exp=='SE'):
-        exp="SE"
+    skillset=parameters.get("Skillset")
+    skillset=''.join(skillset)
+    practice=parameters.get("Practice")
+    practice=''.join(practice)
+    if (exp == '2+ years' or exp == '3+ years' or exp == '4+ years' or exp == '1+ years' or exp == 'SE'):
+        exp = "SE"
     if (exp == '5+ years' or exp == '6+ years' or exp == '7+ years' or exp == 'SSE'):
-        exp='SSE'
+        exp = 'SSE'
     if (exp == '8+ years' or exp == '9+ years' or exp == 'LSE'):
-        exp='LSE'
+        exp = 'LSE'
     if (exp == '11+ years' or exp == '10+ years' or exp == 'ATM'):
-        exp='ATM'
-    if (exp == '11+ years' or exp == '12+ years' or  exp == 'Manager'):
-        exp='Manager'
-    data.append({'practice':'IBM','experience':'SE','skillset':'Java','Position':'02'})
-    data.append({'practice': 'CIS', 'experience': 'SSE', 'skillset': 'python','Position':'01'})
-    data.append({'practice': 'Convergens', 'experience': 'LSE', 'skillset': '.net','Position':'04'})
-    data.append({'practice': 'IBM', 'experience': 'ATM', 'skillset': 'java','Position':'04'})
-    data.append({'practice': 'CIS', 'experience': 'Manager', 'skillset': 'java','Position':'06'})
-    data.append({'practice': 'SMAC', 'experience': 'SE', 'skillset': 'python','Position':'02'})
-    data.append({'practice': 'Kpoint', 'experience': 'LSE', 'skillset': 'java','Position':'03'})
-    data.append({'practice': 'SMAC', 'experience': 'ATM', 'skillset': 'java','Position':'08'})
-    str="We have no such oppurtunities right now. But we will get back to you when we have such openings"
+        exp = 'ATM'
+    if (exp == '11+ years' or exp == '12+ years' or exp == 'Manager'):
+        exp = 'Manager'
+    data=[]
+    data.append({'practice': 'IBM', 'experience': 'SE', 'skillset': 'Java', 'Position': '02'})
+    data.append({'practice': 'CIS', 'experience': 'SSE', 'skillset': 'python', 'Position': '01'})
+    data.append({'practice': 'Convergens', 'experience': 'LSE', 'skillset': '.net', 'Position': '04'})
+    data.append({'practice': 'IBM', 'experience': 'ATM', 'skillset': 'java', 'Position': '04'})
+    data.append({'practice': 'CIS', 'experience': 'Manager', 'skillset': 'java', 'Position': '06'})
+    data.append({'practice': 'SMAC', 'experience': 'SE', 'skillset': 'python', 'Position': '02'})
+    data.append({'practice': 'Kpoint', 'experience': 'LSE', 'skillset': 'java', 'Position': '03'})
+    data.append({'practice': 'SMAC', 'experience': 'ATM', 'skillset': 'java', 'Position': '08'})
+    str = "We have no such oppurtunities right now. But we will get back to you when we have such openings"
     for x in data:
-        if (x['practice']==practice):
-            if (x['experience']==exp ):
-                if x['skillset']==skillset:
-                    str="We have oppurtunities in " + practice + " and the position offered will be of " + exp  + 'and skill required is ' + skillset +' No.Of Position '+x['Position']
+        if (x['practice'] == practice):
+            if (x['experience'] == exp):
+                if x['skillset'] == skillset:
+                    str = "We have oppurtunities in " + practice + " and the position offered will be of " + exp + 'and skill required is ' + skillset + ' No.Of Position ' + \
+                          x['Position']
                 else:
-                    str="We have no such oppurtunities right now. But we will get back to you when we have such openings"
+                    str = "We have no such oppurtunities right now. But we will get back to you when we have such openings"
 
-    #return jsonify({'result':str})
+    print("Response:")
+    print(str)
 
     return {
         "speech": str,
         "displayText": str,
+        #"data": {},
+        # "contextOut": [],
         "source": "apiai-onlinestore-shipping"
     }
+
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
